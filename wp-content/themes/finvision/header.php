f8ae90cfca4b65a6029691b88a216c0bc742247a
Konstantin Semenov <kvsemenoff@mail.ru>
2016-06-25 11:35:59 +0400

    firt commit

---
+++ wp-content/themes/finvision/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/libs/wow/animate.css">
+
+
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style2.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style3.css">
+
+	
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/fonts.css">
+
+	<meta content="telephone=no" name="format-detection"/>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<script src="<?php echo get_template_directory_uri(); ?>/libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/libs/owl.carousel/owl.carousel.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/libs/wow/wow.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/responsiveTabs.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.maskedinput.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/common.js"></script>
+	<script>
+    	new WOW().init();
+    </script>
