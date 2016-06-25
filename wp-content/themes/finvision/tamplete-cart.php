8cbfca0cfa39e5b815c9ae9736692eefc8f4696a
hatuhov <hatuhov@yandex.ru>
2016-06-25 14:36:28 +0300

    ssssssssssssss

---
+++ wp-content/themes/finvision/tamplete-cart.php
@@ -0,0 +1,76 @@
+<?php 
+/* 
+Template name: Карточка товара
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<section class="content-inner">
+	<div class="container">
+		<div class="row no-gutter">   <!-- ********************  no-gutter -->
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<ul class="ah-cart-list1">
+					<li class="ah-cart-list1_item1">Главная - </li>
+					<li class="ah-cart-list1_item2">Контактная информация</li>
+				</ul>
+				<h1>FinnVision — B7</h1>
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<div class="ah-left-block">
+					<img src="img/ah-cart1.jpg" alt="ah-cart1"  class="img-responsive ah-img-block">
+					<ul class="ah-cart-list2">
+						<li>
+							<img src="img/ah-cart2.jpg" alt="ah-cart1"  class="img-responsive ah-img-block1">
+						</li>
+						<li>
+							<img src="img/ah-cart1.jpg" alt="ah-cart1"  class="img-responsive ah-img-block2">
+						</li>
+						<li>
+							<img src="img/ah-cart1.jpg" alt="ah-cart1"  class="img-responsive ah-img-block3">
+						</li>
+					</ul>
+					<span class="ah-cart-span">Описание</span>
+					<p class="ah-cart-text">Компания DVR-group – оптово-розничная компания по продаже автомобильной электроники и аксессуаров. Под брендом «DVR-GROUP» объединилась группа экспертов, которые имея многолетний опыт продаж потребительской электроники на российском рынке, задались целью предложить покупателям недорогие, надежные и эффективные продукты для российских водителей.  С 2011 года мы успешно развиваем данное направления, бесконечно анализируя рынок и предлагая лучшие модели из всего раздела авто электроники. Мы постоянно пополняем ассортимент нашего магазинна и приняли решение сделать доступными для массового потребителя системы видеонаблюдения, эхолоты.</p>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<div class="ah-right-block">
+					<ul class="ah-cart-list3">
+						<li>Размер ЖК-экрана – 3.0 inch 16:9 LCD — High Definition</li>
+						<li>Разрешение – Full HD (1280×720 – 1920×1080)</li>
+						<li>Угол обзора – Wide Angle lens 170</li>
+						<li>Датчик движения –  Есть</li>
+						<li>Датчик удара – Есть</li>
+					</ul>
+					<ul class="ah-cart-list4">
+						<li>Ночная съемка – Есть</li>
+						<li>Формат видео – AVI</li>
+						<li>Формат фото – JPEG</li>
+						<li>Дополнительные разъемы – HDMI, AV</li>
+						<li>Карта памяти – microSD, microSDHC</li>
+						<li>Микрофон – Есть (можно отключить)</li>
+					</ul>
+					<ul class="ah-cart-list5">
+						<li>Динамик – Есть (можно отключить)</li>
+						<li>Интерфейс USB – USB 2.0</li>
+					</ul>
+					<a href="#" class="ah-cart-link1">Купить в интернет-магазине</a>
+					<a href="#" class="ah-cart-link2">Купить <strong>в 1 клик</strong></a>
+				</div>
+			</div>	
+		</div>
+
+				<!-- <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(''); ?>
+				
+				<h2><?php the_title(); ?></h2>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+				<?php endwhile; // end of the loop. ?>
+				
+				<?php require_once('advice.php'); ?> -->
+		<!-- <div class="clear"></div> -->
+	</div>
+</section>
+
+<?php get_footer(); ?>
