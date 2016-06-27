198718d7e00bc323a63aef92da4d7801b3efea73
hatuhov <hatuhov@yandex.ru>
2016-06-27 17:01:52 +0300

    dddddddddddddd

--- wp-content/themes/finvision/tamplete-contacts.php
+++ wp-content/themes/finvision/tamplete-contacts.php
@@ -26,7 +26,36 @@ Template name: Контактная информация
 				<span class="ah-contacts-span5">+7 (343) 288-28-25,  +7 (343) 290-45-46</span>
 				<span class="ah-contacts-span6">contactus@namesite.ru</span>
 				<span class="ah-contacts-span7">Схема проезда до офиса</span>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/ah-contacts2.jpg" alt="ah-contacts2" class="img-responsive ah-img-contactblock1">
+				<!-- <div class="ah-img-contactblock1"> -->
+					<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+					<script type="text/javascript">
+					ymaps.ready(init);
+					 var myMap,
+					     myPlacemark;
+
+					 function init(){     
+					     myMap = new ymaps.Map("map", {
+					         center: [55.73521577724309,37.6503515],
+					         zoom: 16,
+					         controls: []
+					     });
+
+					    myMap.behaviors.disable('scrollZoom'); 
+
+					     var myPlacemark = new ymaps.Placemark([55.73521577724309,37.6503515], {}, {
+					      iconLayout: 'default#image',
+					      iconImageHref: '/img/balun.png',
+					      iconImageSize: [48, 57],
+					      iconImageOffset: [-3, -42]
+					  });
+
+
+					     myMap.geoObjects.add(myPlacemark);
+					 }
+					</script>
+					<div id="map">hhhh <br>kjkjkjk</div>
+				<!-- </div> -->
+<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/ah-contacts2.jpg" alt="ah-contacts2" class="img-responsive ah-img-contactblock1"> -->
 			</div>	
 		</div>
 	</div>
